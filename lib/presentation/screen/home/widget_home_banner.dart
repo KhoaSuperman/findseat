a9677a5ef017662660b41e04558b11e986361f44
khoaha <hoanganhkhoa1012@gmail.com>
2019-12-22 15:51:03 +0700

    [Home] add home banner

--- lib/presentation/screen/home/widget_home_banner.dart
+++ lib/presentation/screen/home/widget_home_banner.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class WidgetHomeBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Image.asset("images/banner1.png",
+        fit: BoxFit.contain,
+      ),
+    );
   }
 }
