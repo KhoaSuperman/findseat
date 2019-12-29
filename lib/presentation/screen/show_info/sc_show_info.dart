57d960c1912328d8d1cffe6c6c9d5f46c827340f
khoaha <hoanganhkhoa1012@gmail.com>
2019-12-29 17:12:45 +0700

    [ShowInfo] basically add button book seat

--- lib/presentation/screen/show_info/sc_show_info.dart
+++ lib/presentation/screen/show_info/sc_show_info.dart
@@ -1,4 +1,5 @@
 import 'package:find_seat/presentation/common_widgets/barrel_common_widgets.dart';
+import 'package:find_seat/presentation/custom_ui/custom_ui.dart';
 import 'package:find_seat/presentation/screen/show_info/barrel_show_info.dart';
 import 'package:find_seat/utils/my_const/my_const.dart';
 import 'package:flutter/material.dart';
@@ -33,9 +34,26 @@ class ShowInfoScreen extends StatelessWidget {
   _buildBtnBookSeat() {
     return Positioned(
       bottom: 0,
-      child: Placeholder(
-        color: COLOR_CONST.DEFAULT,
-        fallbackHeight: 54,
+      right: 0,
+      left: 0,
+      child: Container(
+        height: 54,
+        child: FlatButton(
+          color: COLOR_CONST.DEFAULT,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              MySvgImage(
+                width: 18.25,
+                height: 16.1,
+                path: 'assets/ic_sofa.svg',
+              ),
+              WidgetSpacer(width: 5),
+              Text('Book seats', style: FONT_CONST.MEDIUM_WHITE_16),
+            ],
+          ),
+          onPressed: () {},
+        ),
       ),
     );
   }
