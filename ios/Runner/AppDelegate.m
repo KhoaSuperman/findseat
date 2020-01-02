03c902d3a32efd0aea022173ef505a9679c758bc
khoaha <hoanganhkhoa1012@gmail.com>
2020-01-02 22:05:46 +0700

    config ios google map

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyAh9zLZ2W2yMW_R0eMxH-thhONdi3XZ_aY"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Poppins',
       ),
       onGenerateRoute: Router.generateRoute,
-      initialRoute: Router.BOOK_TIME_SLOT,
+      initialRoute: Router.LOGIN,
     );
   }
 }
