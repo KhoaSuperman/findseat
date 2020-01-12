01559bd5a958364cb7381a2f0f8ddb6e98f0e49b
khoaha <hoanganhkhoa1012@gmail.com>
2020-01-12 15:22:29 +0700

    [PaymentMethod] handle payment with card form

--- lib/presentation/screen/payment_method_picker/sc_payment_method_picker.dart
+++ lib/presentation/screen/payment_method_picker/sc_payment_method_picker.dart
@@ -2,6 +2,7 @@ import 'package:find_seat/presentation/common_widgets/barrel_common_widgets.dart
 import 'package:find_seat/presentation/custom_ui/custom_ui.dart';
 import 'package:find_seat/utils/my_const/my_const.dart';
 import 'package:flutter/material.dart';
+import 'package:stripe_payment/stripe_payment.dart';
 
 class PaymentMethodPickerScreen extends StatefulWidget {
   @override
@@ -10,6 +11,16 @@ class PaymentMethodPickerScreen extends StatefulWidget {
 }
 
 class _PaymentMethodPickerScreenState extends State<PaymentMethodPickerScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    StripePayment.setOptions(StripeOptions(
+        publishableKey: "pk_test_ZeDmYBSVOpxm7bwr7rNvOz8v",
+        merchantId: "Test",
+        androidPayMode: 'test'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -36,25 +47,30 @@ class _PaymentMethodPickerScreenState extends State<PaymentMethodPickerScreen> {
               break;
           }
 
-          return Container(
-            height: 70,
-            child: Row(
-              children: <Widget>[
-                index != 1
-                    ? MySvgImage(
-                        height: 40,
-                        width: 40,
-                        path: iconPath,
-                        applyColorFilter: false,
-                      )
-                    : Image.asset(
-                        iconPath,
-                        width: 40,
-                        height: 40,
-                      ),
-                WidgetSpacer(width: 14),
-                Text(name, style: FONT_CONST.REGULAR_GRAY4_14),
-              ],
+          return GestureDetector(
+            onTap: () {
+              _handleMethodClick(index);
+            },
+            child: Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  index != 1
+                      ? MySvgImage(
+                          height: 40,
+                          width: 40,
+                          path: iconPath,
+                          applyColorFilter: false,
+                        )
+                      : Image.asset(
+                          iconPath,
+                          width: 40,
+                          height: 40,
+                        ),
+                  WidgetSpacer(width: 14),
+                  Text(name, style: FONT_CONST.REGULAR_GRAY4_14),
+                ],
+              ),
             ),
           );
         },
@@ -64,5 +80,63 @@ class _PaymentMethodPickerScreenState extends State<PaymentMethodPickerScreen> {
         itemCount: 3,
       ),
     );
+  }
+
+  void _handleMethodClick(int index) {
+    _cardForm();
+  }
+
+  void _webPayment() {
+    var onError = (error) {
+      print('Error. ${error.toString()}');
+    };
+
+    StripePayment.createSourceWithParams(SourceParams(
+      type: 'ideal',
+      amount: 1099,
+      currency: 'eur',
+      returnURL: 'example://stripe-redirect',
+    )).then((source) {
+      print('Received ${source.sourceId}');
+    }).catchError(onError);
+  }
+
+  void _cardForm() {
+    var onError = (error) {
+      print('Error. ${error.toString()}');
+    };
+
+    StripePayment.paymentRequestWithCardForm(CardFormPaymentRequest())
+        .then((paymentMethod) {
+      print(
+          'Received type=${paymentMethod.type}, id=${paymentMethod.billingDetails.toJson()}');
+    }).catchError(onError);
+  }
+
+  void _nativePayment() {
+    var onError = (error) {
+      print('Error. ${error.toString()}');
+    };
+
+    StripePayment.paymentRequestWithNativePay(
+      androidPayOptions: AndroidPayPaymentRequest(
+        total_price: "1.20",
+        currency_code: "EUR",
+      ),
+      applePayOptions: ApplePayPaymentOptions(
+        countryCode: 'DE',
+        currencyCode: 'EUR',
+        items: [
+          ApplePayItem(
+            label: 'Test',
+            amount: '13',
+          )
+        ],
+      ),
+    ).then((token) {
+      setState(() {
+        print('Token: ${token.toJson()}');
+      });
+    }).catchError(onError);
   }
 }
