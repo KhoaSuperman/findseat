7df8f834919abfc54f19ee3379e6ee71695d5b03
khoaha <hoanganhkhoa1012@gmail.com>
2020-03-01 14:12:03 +0700

    [Home] refactor display loading/error message

--- lib/presentation/screen/home/sc_home.dart
+++ lib/presentation/screen/home/sc_home.dart
@@ -22,41 +22,63 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return BlocBuilder<HomeBloc, HomeState>(
       builder: (context, state) {
-        if (state is HomeLoading) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (state is HomeLoaded) {
-          return Scaffold(
+        return SafeArea(
+          child: Scaffold(
             body: Container(
               color: COLOR_CONST.WHITE,
-              child: ListView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
                   WidgetHomeToolbar(),
-                  WidgetHomeBanner(),
-                  WidgetSpacer(height: 30),
-                  WidgetHomeCategories(),
-                  WidgetSpacer(height: 30),
-                  WidgetRecommendedSeats(),
-                  WidgetSpacer(height: 30),
-                  WidgetNearbyTheatres(),
-                  WidgetSpacer(height: 30),
-                  WidgetHomeEvents(),
-                  WidgetSpacer(height: 30),
-                  WidgetHomePlays(),
-                  WidgetSpacer(height: 30),
+                  _buildContent(state),
                 ],
               ),
             ),
-          );
-        } else if (state is HomeNotLoaded) {
-          return Center(
-            child: Text('Cannot load data'),
-          );
-        } else {
-          return Text('Unknown state');
-        }
+          ),
+        );
       },
     );
+  }
+
+  Widget _buildContent(HomeState state) {
+    if (state is HomeLoaded) {
+      return Expanded(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            WidgetHomeBanner(),
+            WidgetSpacer(height: 30),
+            WidgetHomeCategories(),
+            WidgetSpacer(height: 30),
+            WidgetRecommendedSeats(),
+            WidgetSpacer(height: 30),
+            WidgetNearbyTheatres(),
+            WidgetSpacer(height: 30),
+            WidgetHomeEvents(),
+            WidgetSpacer(height: 30),
+            WidgetHomePlays(),
+            WidgetSpacer(height: 30),
+          ],
+        ),
+      );
+    } else if (state is HomeLoading) {
+      return Expanded(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else if (state is HomeNotLoaded) {
+      return Expanded(
+        child: Center(
+          child: Text('Cannot load data'),
+        ),
+      );
+    } else {
+      return Expanded(
+        child: Center(
+          child: Text('Unknown state'),
+        ),
+      );
+    }
   }
 }
