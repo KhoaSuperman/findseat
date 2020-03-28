7db37fe5ed2817b976379a2f4de7164879597817
khoaha <hoanganhkhoa1012@gmail.com>
2020-03-28 15:09:32 +0700

    format bloc delegate data

--- lib/app/simple_bloc_delegate.dart
+++ lib/app/simple_bloc_delegate.dart
@@ -1,21 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:intl/intl.dart';
 
 class SimpleBlocDelegate extends BlocDelegate {
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
-    print(event);
+    log('onEvent', event);
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stacktrace) {
     super.onError(bloc, error, stacktrace);
-    print(error);
+    log('onError', error);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    log(
+        'onTransition',
+        '\tcurrentState=${transition.currentState}\n'
+            '\tnextState=${transition.nextState}');
+  }
+
+  void log(String name, Object msg) {
+    print(
+        '===== ${DateFormat("HH:mm:ss-dd MMM, yyyy").format(DateTime.now())}: $name\n'
+        '$msg');
   }
 }
